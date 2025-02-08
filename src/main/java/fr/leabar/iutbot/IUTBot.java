6d417b24bb8d0bc46275ca0cf0da33d76ec876ea
Léa BAR <contact@0xlea.fr>
2025-02-08 01:30:43 +0100

    [Implement] Implement Module System to Main Class

--- src/main/java/fr/leabar/iutbot/IUTBot.java
+++ src/main/java/fr/leabar/iutbot/IUTBot.java
@@ -1,9 +1,18 @@
 package fr.leabar.iutbot;
 
 import fr.leabar.iutbot.config.ConfigManager;
+import fr.leabar.iutbot.modules.ModuleManager;
 
 public class IUTBot {
     public static void main(String[] args) {
-
+        ModuleManager.registerModules();
+        ModuleManager.startModules();
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run() {
+                ModuleManager.stopModules();
+            }
+        });
     }
 }
